ab5a7d398ca563271394b67c626bd96657794800
Anas Arbaoui <v3e9ge7pi@relay.firefox.com>
2020-09-11 16:59:20 +0100

    master

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from skgen import SkGen
 
+sk = SkGen(64)
 app = Flask(__name__)
+app.secret_key = sk.gen()
 
 
 @app.route('/', methods=["GET", "POST"])
 def login():
     log_current = True
+    email1 = 'admin@localhost.com'
+    password1 = 'admin'
     if request.method == "POST":
         req = request.form
         email = email = req.get('email')
@@ -15,9 +20,15 @@ def login():
             if v == "":
                 missing.append(k)
         if missing:
-            feedback = f"Missing input for {', '.join(missing)}"
+            feedback = f"missing input for {', '.join(missing)}"
             return render_template("public/login.html", feedback=feedback, log_current=log_current)
-        return "OK"
+        session['email'] = email
+        session['password'] = password
+        if email1 == email and password1 == password:
+            return "OK"
+        else:
+            feedback = f'Wrong password or email'
+            return render_template("public/login.html", feedback=feedback, log_current=log_current)
     return render_template('public/login.html', log_current=log_current)
 
 
@@ -37,7 +48,7 @@ def register():
             if v == "":
                 missing.append(k)
         if missing:
-            feedback = f"Missing input for {', '.join(missing)}"
+            feedback = f"missing input for {', '.join(missing)}"
             return render_template("public/register.html", feedback=feedback, reg_current=reg_current)
         else:
             # Adds the data from the form to the db and redirects to the login page
@@ -45,6 +56,15 @@ def register():
             return redirect(url_for('login'))
     else:
         return render_template('public/register.html', reg_current=reg_current)
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'email' and 'password' in session:
+        email = session['email']
+        return render_template('public/dashboard.html', email=email)
+    else:
+        return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
