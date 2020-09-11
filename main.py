cb687b1ed76c151e6526c086986724e193e26a51
Anas Arbaoui <v3e9ge7pi@relay.firefox.com>
2020-09-11 16:14:54 +0100

    master

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def login():
+    log_current = True
     if request.method == "POST":
         req = request.form
         email = email = req.get('email')
@@ -15,13 +16,14 @@ def login():
                 missing.append(k)
         if missing:
             feedback = f"Missing input for {', '.join(missing)}"
-            return render_template("public/login.html", feedback=feedback)
+            return render_template("public/login.html", feedback=feedback, log_current=log_current)
         return "OK"
-    return render_template('public/login.html')
+    return render_template('public/login.html', log_current=log_current)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    reg_current = True
     if request.method == "POST":
         # Gets POST request data
         req = request.form
@@ -36,13 +38,13 @@ def register():
                 missing.append(k)
         if missing:
             feedback = f"Missing input for {', '.join(missing)}"
-            return render_template("public/register.html", feedback=feedback)
+            return render_template("public/register.html", feedback=feedback, reg_current=reg_current)
         else:
             # Adds the data from the form to the db and redirects to the login page
             # add SQL support here
             return redirect(url_for('login'))
     else:
-        return render_template('public/register.html')
+        return render_template('public/register.html', reg_current=reg_current)
 
 
 if __name__ == "__main__":
